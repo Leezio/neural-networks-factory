254cebfd083bb0c82c897a809dd268bd95ecbb5a
François Flament <contact@leezio.fr>
2019-05-02 09:21:10 +0200

    Edit view.py

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -9,18 +9,21 @@ import matplotlib.pyplot as plt
 from IPython.display import set_matplotlib_formats
 set_matplotlib_formats('svg')
 
+
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
 
+
 def neuralNetwork(request, sentence):
     imdb = keras.datasets.imdb
-    (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+    (train_data, train_labels), (test_data,
+                                 test_labels) = imdb.load_data(num_words=10000)
 
     ##############################
-    
+
     word_index = imdb.get_word_index()
-    test=[]
+    test = []
     for word in keras.preprocessing.text.text_to_word_sequence(sentence, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' '):
         if word in word_index:
             test.append(word_index[word])
@@ -29,9 +32,11 @@ def neuralNetwork(request, sentence):
 
     ##############################
 
-    train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=0, padding='post', maxlen=256)
+    train_data = keras.preprocessing.sequence.pad_sequences(
+        train_data, value=0, padding='post', maxlen=256)
 
-    test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=0, padding='post', maxlen=256)
+    test_data = keras.preprocessing.sequence.pad_sequences(
+        test_data, value=0, padding='post', maxlen=256)
 
     vocab_size = 10000
 
@@ -43,19 +48,26 @@ def neuralNetwork(request, sentence):
     model.add(keras.layers.Dense(16, activation=tf.nn.relu))
     model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
 
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-              
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
     x_val = train_data[:10000]
     partial_x_train = train_data[10000:]
 
     y_val = train_labels[:10000]
-    partial_y_train = train_labels[10000:]   
+    partial_y_train = train_labels[10000:]
 
-    model.fit(partial_x_train, partial_y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+    model.fit(partial_x_train, partial_y_train, epochs=40,
+              batch_size=512, validation_data=(x_val, y_val), verbose=1)
 
-    score, accuracy = model.evaluate(test_data, test_labels, batch_size=512, verbose=1)   
-    
+    score, accuracy = model.evaluate(
+        test_data, test_labels, batch_size=512, verbose=1)
+
     prediction = model.predict_classes(test, verbose=1)
 
-    return JsonResponse({'TensorFlow': str(tf.__version__),"Sentence": sentence, "Result": {"Score": str(score), "Accuracy": str(accuracy), "Prediction": str(prediction)}})
+    if (prediction[0][0] == 1):
+        prediction = "Positive"
+    else:
+        prediction = "Negative"
 
+    return JsonResponse({'TensorFlow': str(tf.__version__), "Sentence": sentence, "Result": {"Score": str(score), "Accuracy": str(accuracy), "Prediction": prediction}})
