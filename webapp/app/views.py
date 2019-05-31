ef1abc8607af160202b4ee0344197b06d17bd90e
François Flament <contact@leezio.fr>
2019-05-31 21:30:49 +0200

    Add pretrained models

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -4,10 +4,6 @@ from django.template import loader
 
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
-import matplotlib.pyplot as plt
-from IPython.display import set_matplotlib_formats
-set_matplotlib_formats('svg')
 
 
 def index(request):
@@ -17,52 +13,23 @@ def index(request):
 
 def neuralNetwork(request, sentence):
     imdb = keras.datasets.imdb
-    (train_data, train_labels), (test_data,
-                                 test_labels) = imdb.load_data(num_words=10000)
 
-    ##############################
+    sentence = "I hate this movie"
 
     word_index = imdb.get_word_index()
-    test = []
+
+    test=[]
+
     for word in keras.preprocessing.text.text_to_word_sequence(sentence, filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=' '):
         if word in word_index:
             test.append(word_index[word])
 
     test = keras.preprocessing.sequence.pad_sequences([test], maxlen=256)
 
-    ##############################
+    model = keras.models.load_model('my_model.h5')
 
-    train_data = keras.preprocessing.sequence.pad_sequences(
-        train_data, value=0, padding='post', maxlen=256)
-
-    test_data = keras.preprocessing.sequence.pad_sequences(
-        test_data, value=0, padding='post', maxlen=256)
-
-    vocab_size = 10000
-
-    model = keras.Sequential()
-    model.add(keras.layers.Embedding(vocab_size, 16))
-    model.add(keras.layers.GlobalAveragePooling1D())
-    model.add(keras.layers.Dense(16, activation=tf.nn.relu))
-    model.add(keras.layers.Dense(16, activation=tf.nn.relu))
-    model.add(keras.layers.Dense(16, activation=tf.nn.relu))
-    model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
-
-    model.compile(optimizer='adam', loss='binary_crossentropy',
-                  metrics=['accuracy'])
-
-    x_val = train_data[:10000]
-    partial_x_train = train_data[10000:]
-
-    y_val = train_labels[:10000]
-    partial_y_train = train_labels[10000:]
-
-    model.fit(partial_x_train, partial_y_train, epochs=40,
-              batch_size=512, validation_data=(x_val, y_val), verbose=1)
-
-    score, accuracy = model.evaluate(
-        test_data, test_labels, batch_size=512, verbose=1)
-
+    model.summary()
+    
     prediction = model.predict_classes(test, verbose=1)
 
     if (prediction[0][0] == 1):
@@ -70,4 +37,4 @@ def neuralNetwork(request, sentence):
     else:
         prediction = "Negative"
 
-    return JsonResponse({'TensorFlow': str(tf.__version__), "Sentence": sentence, "Result": {"Score": str(score), "Accuracy": str(accuracy), "Prediction": prediction}})
+    return JsonResponse({'TensorFlow': str(tf.__version__),"Sentence": sentence, "Result": {"Prediction": prediction}})
